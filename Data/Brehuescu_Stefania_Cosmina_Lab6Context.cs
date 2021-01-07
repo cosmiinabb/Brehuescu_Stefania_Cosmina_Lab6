081181127e307ed43bff96915433b233a60d5684
Ștefania Brehuescu <cosmina.b6@gmail.com>
2021-01-07 02:02:36 +0200

    Add project files.

---
+++ Data/Brehuescu_Stefania_Cosmina_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Brehuescu_Stefania_Cosmina_Lab6.Models;
+
+namespace Brehuescu_Stefania_Cosmina_Lab6.Data
+{
+    public class Brehuescu_Stefania_Cosmina_Lab6Context : DbContext
+    {
+        public Brehuescu_Stefania_Cosmina_Lab6Context (DbContextOptions<Brehuescu_Stefania_Cosmina_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brehuescu_Stefania_Cosmina_Lab6.Models.Book> Book { get; set; }
+    }
+}
